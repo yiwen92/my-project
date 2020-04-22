38f129655b4d17b5e01af5ef09264c7e8f570419
zouning68 <zouning9168@163.com>
2020-04-22 17:55:03 +0800

    Update docker.sh

--- consult_recommend_http/script/docker.sh
+++ consult_recommend_http/script/docker.sh
@@ -1,3 +1,4 @@
+# https://tensorflow.google.cn/tfx/serving/serving_basic
 #TESTDATA="$(pwd)/serving/tensorflow_serving/servables/tensorflow/testdata"
 #docker run -t --rm -p 8501:8501 -v "$TESTDATA/saved_model_half_plus_two_cpu:/models/half_plus_two" -e MODEL_NAME=half_plus_two tensorflow/serving &
 
