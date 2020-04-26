22a74c45c21d64a33a7627982fd21633703906bc
zouning68 <zouning9168@163.com>
2020-04-26 14:29:52 +0800

    Add files via upload

--- embedding-all/request_serving_model.py
+++ embedding-all/request_serving_model.py
@@ -1,53 +1,38 @@
 #coding:utf8
 #curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://localhost:8501/v1/models/half_plus_two:predict
 
-import json, requests, sys, pickle, heapq
+import json, requests, sys, pickle, heapq, random
 import tensorflow.contrib.keras as kr
+from data_utils import gen_train_samples
+from config import FLAGS
+import numpy as np
+import tensorflow as tf
 
-sentence_class_ip = '192.168.7.205:8511'
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
 
-#URL = 'http://localhost:8501/v1/models/sentence_class:predict'
-#URL = 'http://192.168.7.218:8501/v1/models/sentence_class:predict'
-#URL = 'http://192.168.7.205:8511/v1/models/sentence_class:predict'
-URL = 'http://' + sentence_class_ip + '/v1/models/sentence_class:predict'
+# 原始的样本数据
+valid_data = gen_train_samples(FLAGS.valid_samples)
+entity_ids, entity_ids_list = np.array(valid_data[0], dtype="int32"), np.array(valid_data[1], dtype="int32")
+# 人造的数据
+entity_ids = np.random.randint(0, 1000, size=[6, 10])
+entity_ids_list = np.random.randint(0, 1000, size=[6, 15, 10])
 
-try:
-    sentence = sys.argv[1]
-except:
-    sentence = '找工作'
+sen_x_pad = [random.sample([i for i in range(1000)], 200)]
+#sen_data = json.dumps({"signature_name": "serving_default", "instances": sen_x_pad})
+sen_data = json.dumps({"signature_name": "serving_default", "instances": [{"input": sen_x_pad}]})
+sen_data = json.dumps({"instances": sen_x_pad})
+sen_json_response = requests.post('http://192.168.7.218:8511/v1/models/sentence_class:predict', data=sen_data)
 
-'''
-test_input = [[0] * 195 + [5, 11, 30, 80]]
-data = json.dumps({"signature_name": "serving_default", "instances": test_input})
-print 'Data : ', data
-headers = {"content-type": "application/json"}
-json_response = requests.post('http://localhost:8501/v1/models/sentence_class:predict', data=data, headers=headers)
+ent_data = json.dumps({"instances": [{"entity_ids": entity_ids[0], "entity_ids_list": entity_ids_list[0]}, \
+    {"entity_ids": entity_ids[1], "entity_ids_list": entity_ids_list[1]}]}, cls=NumpyEncoder)
+json_response = requests.post('http://192.168.7.218:8512/v1/models/embedding_entity:predict', data=ent_data)
 predictions = json.loads(json_response.text)['predictions']
-print 'predictions : ', predictions
-'''
 
-class Predictor():
-    def __init__(self):
-        self.map_path = './model/id.maps'
-        with open(self.map_path, "rb") as f:
-            self.word_to_id, self.cat_to_id, self.seq_length, self.num_classes = pickle.load(f)
-        self.id_to_cat = {v: k for k, v in self.cat_to_id.items()}
-    def predict(self, txt, topk=1):
-        #print json.dumps(self.word_to_id, ensure_ascii=False), json.dumps(self.cat_to_id, ensure_ascii=False), self.seq_length, self.num_classes
-        data_id = [[self.word_to_id[x] for x in list(txt.decode('utf-8')) if x in self.word_to_id]] #; print data_id
-        x_pad = kr.preprocessing.sequence.pad_sequences(data_id, self.seq_length).tolist()   #; print x_pad;exit()
-        data = json.dumps({"signature_name": "serving_default", "instances": x_pad})
-        headers = {"content-type": "application/json"}
-        json_response = requests.post(URL, data=data, headers=headers)
-        predictions = json.loads(json_response.text)['predictions'] #;   print 'predictions : ', predictions
-        pred_prob = predictions[0]  #; print pred_prob
-        k_index = list(map(pred_prob.index, heapq.nlargest(topk, pred_prob)))
-        k_value = heapq.nlargest(topk, pred_prob)
-        res = []
-        for i in range(len(k_index)):
-            res.append((self.id_to_cat[k_index[i]], round(k_value[i], 3)))
-        return res#[0]   # json.dumps({'sentence': txt, 'predict_result': res}, ensure_ascii=False)     # res[0]
+a=1
 
 if __name__ == '__main__':
-    p = Predictor()
-    print(json.dumps(p.predict(sentence), ensure_ascii=False))
+    pass
