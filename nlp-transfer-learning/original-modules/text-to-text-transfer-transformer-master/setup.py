aa4a2429ad3f9158df67f5e34e205cda36bb552d
zouning68 <zouning9168@163.com>
2020-05-11 17:54:11 +0800

    Add files via upload

---
+++ nlp-transfer-learning/original-modules/text-to-text-transfer-transformer-master/setup.py
@@ -0,0 +1,76 @@
+# Copyright 2019 The T5 Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Install T5."""
+
+import setuptools
+
+# Get the long description from the README file.
+with open('README.md') as fp:
+  _LONG_DESCRIPTION = fp.read()
+
+setuptools.setup(
+    name='t5',
+    version='0.1.6',
+    description='Text-to-text transfer transformer',
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Google Inc.',
+    author_email='no-reply@google.com',
+    url='http://github.com/google-research/text-to-text-transfer-transformer',
+    license='Apache 2.0',
+    packages=setuptools.find_packages(),
+    package_data={
+        '': ['*.gin'],
+    },
+    scripts=[],
+    install_requires=[
+        'absl-py',
+        'allennlp',
+        'babel',
+        'future',
+        'gin-config',
+        'mesh-tensorflow[transformer]>=0.1.7',
+        'nltk',
+        'numpy',
+        'pandas',
+        'rouge-score',
+        'sacrebleu',
+        'scikit-learn',
+        'scipy',
+        'sentencepiece',
+        'six',
+        'tensorflow-datasets>=1.3.2',
+        'tensorflow-text==1.15.0rc0',
+    ],
+    extras_require={
+        'tensorflow': ['tensorflow==1.15'],
+        'gcp': ['gevent', 'google-api-python-client', 'google-compute-engine',
+                'google-cloud-storage', 'oauth2client'],
+    },
+    entry_points={
+        'console_scripts': [
+            't5_mesh_transformer = '
+            't5.models.mesh_transformer_main:console_entry_point',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    keywords='text nlp machinelearning',
+)
